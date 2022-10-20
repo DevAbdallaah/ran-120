49adfd8b9e48316489b425c21eeaeaeb2cd158c7
Abdallah Seebawai <100685261+DevAbdallaah@users.noreply.github.com>
2022-10-19 21:09:48 -0700

    Add files via upload

---
+++ Run.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+
+int main()
+{
+//MULTIPLICATION TABLE
+ int i, j;
+  for(i=0; i<=100; i++){
+    for(j=0;j<=25;j++){
+     cout<<i<<" * "<<j<<" = "<<i*j<<endl;}
+     cout<<endl;
+
+    }
+
+
+ system("pause");
+ return 0;
+}
